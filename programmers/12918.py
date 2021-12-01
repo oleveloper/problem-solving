3b4ab6193a8db7a870cabdb24c83cdeda889daed
Youngeun Lee <83587720+oleveloper@users.noreply.github.com>
2021-12-01 11:52:45 +0900

    Create 12918.py

---
+++ programmers/12918.py
@@ -0,0 +1,12 @@
+# 문자열 다루기 기본
+# https://programmers.co.kr/learn/courses/30/lessons/12918
+
+def solution(s):
+    answer = True
+    
+    if s.isdigit() and len(s) == 4 or len(s) == 6:
+        answer = True
+    else:
+        answer = False
+    
+    return answer
