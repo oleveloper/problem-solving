452d4e8736e7376f0505b6b4d63245e803e24ae0
Youngeun Lee <83587720+oleveloper@users.noreply.github.com>
2021-12-04 22:28:00 +0900

    Create 77884.py

---
+++ programmers/77884.py
@@ -0,0 +1,15 @@
+# 약수의 개수와 덧셈
+# https://programmers.co.kr/learn/courses/30/lessons/77884
+
+def solution(left, right):
+    answer = 0
+    
+    for i in range(left, right + 1):
+        cnt = 1
+        for j in range(2, i + 1):
+            if i % j == 0: cnt += 1
+        
+        if cnt % 2 == 0: answer += i
+        else: answer -= i
+    
+    return answer
