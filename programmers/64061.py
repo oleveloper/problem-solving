b9548e24657ad15f994c3f460b46a86b00e6bd84
Youngeun Lee <83587720+oleveloper@users.noreply.github.com>
2021-12-05 21:02:55 +0900

    Create 64061.py

---
+++ programmers/64061.py
@@ -0,0 +1,19 @@
+# 크레인 인형뽑기 게임
+# https://programmers.co.kr/learn/courses/30/lessons/64061
+
+def solution(board, moves):
+    answer = 0
+    stack = []
+
+    for i in moves:
+        for j in range(len(board)):
+            if board[j][i - 1] != 0:
+                stack.append(board[j][i - 1])
+                board[j][i - 1] = 0
+                if len(stack) > 1 and stack[-1] == stack[-2]:
+                    print(stack)
+                    stack = stack[:-2]
+                    answer += 2
+                break
+
+    return answer
