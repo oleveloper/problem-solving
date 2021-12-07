6d79de3c00c5814261f25b90c37647af28205ea9
Youngeun Lee <83587720+oleveloper@users.noreply.github.com>
2021-12-07 14:04:32 +0900

    Remove print()

--- programmers/64061.py
+++ programmers/64061.py
@@ -11,7 +11,6 @@ def solution(board, moves):
                 stack.append(board[j][i - 1])
                 board[j][i - 1] = 0
                 if len(stack) > 1 and stack[-1] == stack[-2]:
-                    print(stack)
                     stack = stack[:-2]
                     answer += 2
                 break
