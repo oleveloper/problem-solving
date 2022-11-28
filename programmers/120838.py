120159c796ae4b5fb4c3d5b1363c58c3ded69413
Youngeun Lee <oleveloper@gmail.com>
2022-11-28 22:23:44 +0900

    Create 120838.py

---
+++ programmers/120838.py
@@ -0,0 +1,17 @@
+# 모스부호 (1)
+# https://school.programmers.co.kr/learn/courses/30/lessons/120838
+
+def solution(letter):
+    answer = ''
+    morse = { 
+        '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+        '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+        '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+        '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+        '-.--':'y','--..':'z'
+    }
+    
+    for x in letter.split(' '):
+        answer += morse[x]
+        
+    return answer
