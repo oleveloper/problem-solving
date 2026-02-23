ca41a535790834b770ffebec5e150f8d6336a504
oleveloper <oleveloper@gmail.com>
2026-02-23 10:58:38 +0900

    Update move-zeroes.py

--- leetcode/move-zeroes.py
+++ leetcode/move-zeroes.py
@@ -4,7 +4,6 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         for i in range(len(nums))[::-1]:
-            print(i, nums[i])
             if nums[i] == 0:
                 nums.pop(i)
                 nums.append(0)
